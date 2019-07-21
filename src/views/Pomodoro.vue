94750b1dd4f6c873e3c32aa7be3bcc89cba76666
歐津柏 <oujinbai@oujinbais-MacBook-Pro-2.local>
2019-07-22 00:36:27 +0800

    week 1 page 1

--- src/views/Pomodoro.vue
+++ src/views/Pomodoro.vue
@@ -56,31 +56,51 @@
         </div>
       </div>
       <div class="pomodoro__main">
-        <div class="pomodoro__main-timer">
-          <div class="pomodoro__main-inner-circle">
-            <div class="pomodoro__main-time-display">
-              25:00
+        <div class="pomodoro__main-left">
+          <div class="pomodoro__main-timer">
+            <div class="pomodoro__main-inner-circle">
+              <div class="pomodoro__main-time-display">
+                25:00
+              </div>
+            </div>
+          </div>
+          <div class="pomodoro__main-task">
+            Current Task
+          </div>
+          <div class="pomodoro__main-controls">
+            <div class="pomodoro__main-icon">
+              <v-icon x-large>
+                fas fa-fast-forward
+              </v-icon>
+            </div>
+            <div class="pomodoro__main-icon">
+              <v-icon x-large>
+                fas fa-play
+              </v-icon>
+            </div>
+            <div class="pomodoro__main-icon">
+              <v-icon x-large>
+                fas fa-bell
+              </v-icon>
             </div>
           </div>
         </div>
-        <div class="pomodoro__main-task">
-          Current Task
-        </div>
-        <div class="pomodoro__main-controls">
-          <div class="pomodoro__main-icon">
-            <v-icon x-large>
-              fas fa-fast-forward
-            </v-icon>
+        <div class="pomodoro__main-right">
+          <div class="pomodoro__main-input-wrapper">
+            <input class="pomodoro__main-input" type="text" name="" id=""/>
+            <button class="pomodoro__main-add">+</button>
           </div>
-          <div class="pomodoro__main-icon">
-            <v-icon x-large>
-              fas fa-play
-            </v-icon>
-          </div>
-          <div class="pomodoro__main-icon">
-            <v-icon x-large>
-              fas fa-bell
-            </v-icon>
+          <div class="pomodoro__main-task-wrapper">
+            <div class="task">
+              <div class="task__list" v-for="(task, index) in tasks" :key="index">
+                <label class="task__list-option" :for="`'task_'${ index }`">
+                  {{task.name}}
+                  <input type="checkbox" :name="`'task_'${ index }`" :id="`'task_'${ index }`" checked>
+                  <span class="task__list-check"></span>
+                  <span class="list-checked"></span>
+                </label>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -90,7 +110,19 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-
+  data() {
+    return {
+      tasks: [
+        { name: 'Current Project' },
+        { name: 'Study for exam' },
+        { name: 'Workout' },
+        { name: 'Prepare Presentation' },
+        { name: 'Brainstorming' },
+        { name: '第一關' },
+        { name: 'First Task' }
+      ]
+    }
+  }
 })
 </script>
 
@@ -162,18 +194,43 @@ export default Vue.extend({
         color: #19B3D8
         & i
           color: #19B3D8 !important
-
-
     &__main
+      max-width: 70%
       background-color: #F3F3F3
       flex: 8
       display: flex
-      flex-direction: column
       align-items: center
       justify-content: space-between
       font-size: 100px
-      padding-top: 1rem
       padding-bottom: 1rem
+    &__main-left
+      width: 50%
+      display: flex
+      flex-direction: column
+      align-items: center
+    &__main-right
+      width: 50%
+      height: 100%
+      display: flex
+      flex-direction: column
+      align-items: start
+      justify-content: flex-start
+    &__main-input-wrapper
+      background-color: #fff
+      width: 100%
+      height: 140px
+      margin-bottom: 0.5rem
+      display: flex
+      overflow: hidden
+      padding: 15px
+      & input
+        border-bottom: solid 1px #000
+        &:focus
+          outline: none
+    &__main-input
+      width: 90%
+    &__main-add
+      font-size: 60px
     &__main-timer
       width: 400px
       height: 400px
@@ -201,4 +258,65 @@ export default Vue.extend({
       display: flex
       width: 80%
       justify-content: space-around
+    &__main-task-wrapper
+      width: 100%
+      height: 100%
+      display: flex
+      align-items: center
+      justify-content: center
+      padding: 1rem
+      background-color: #FFF
+  .task
+    width: 80%
+    height: 100%
+    text-align: justify
+    &__list
+      margin: 1rem
+    &__list-option 
+      display: block
+      font-size: 1.25rem
+      position: relative
+      padding-left: 35px
+      margin-bottom: 12px
+      cursor: pointer
+      & input 
+        position: absolute
+        opacity: 0
+        cursor: pointer
+        height: 0
+        width: 0 
+        &:hover + span
+          background-color: #ccc
+        &:checked + span
+          background-color: #12B1BF
+        &:checked ~ span.list-checked
+          display: block
+      & span.list-checked  
+          display: none
+          position: absolute
+          left: 90%
+          top: 0
+          height: 37px
+          width: 37px
+          &::after
+            position: absolute
+            content: ""
+            top: 2px
+            left: 9px
+            width: 10px
+            height: 20px
+            border: solid #BABABA
+            border-width: 0 3px 3px 0
+            -webkit-transform: rotate(45deg)
+            -ms-transform: rotate(45deg)
+            transform: rotate(45deg)
+            background-color: #fff
+    &__list-check
+      position: absolute
+      top: 0
+      left: 0
+      height: 25px
+      width: 25px
+      background-color: #eee
+      border-radius: 50%
 </style>
